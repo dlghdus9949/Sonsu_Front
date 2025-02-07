39a1341bf10d4399aa0e67e29c4dcf7ed5c38c1d
cleame8922 <cleame8922@sungkyul.ac.kr>
2025-02-07 20:11:01 +0900

    [add]폰트 및 이미지 추가

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { customFonts } from "./src/constants/fonts";
 
 export default function App() {
+  const [fontsLoaded] = useFonts(customFonts);
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>해내보자 ..... 세1기 화이팅 ....</Text>
+      <Text style={{ fontFamily: "PretendardVariable", fontSize: 20 }}>
+        안녕하세요, PretendardVariable 입니다!
+      </Text>
+      <Text style={{ fontFamily: "RixInooAriDuriRegular", fontSize: 20 }}>
+        안녕하세요, RixInooAriDuriRegular 입니다!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/constants/fonts.js
@@ -0,0 +1,4 @@
+export const customFonts = {
+  "PretendardVariable": require("../../assets/fonts/PretendardVariable.ttf"),
+  "RixInooAriDuriRegular": require("../../assets/fonts/RixInooAriDuri Regular.ttf"),
+};
