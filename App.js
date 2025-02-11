0e55ce139f51d9824facb67a10a32af22aa976c8
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-02-11 17:24:31 +0900

    [add] 하단 Nav

--- App.js
+++ App.js
@@ -1,47 +1,28 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Login from "./src/pages/Login";
-import SignUp from "./src/pages/SignUp";
-import Main from "./src/pages/Main";
+import { GestureHandlerRootView } from "react-native-gesture-handler"; // ✅ 추가
+
+import Login from "./src/screens/Login";
+import SignUp from "./src/screens/SignUp";
 import Menu from "./src/components/Menu";
-import AppPermissions from "./src/pages/AppPermissions";
-import PrivacyPolicy from "./src/pages/PrivacyPolicy";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { useFonts } from "expo-font";
-import { customFonts } from "./src/constants/fonts";
+import AppPermissions from "./src/screens/AppPermissions";
+import PrivacyPolicy from "./src/screens/PrivacyPolicy";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Main" component={Menu} />
-        <Stack.Screen name="AppPermissions" component={AppPermissions} />
-        <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicy} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    //   <View style={styles.container}>
-    //   <Text style={{ fontFamily: "PretendardVariable", fontSize: 20 }}>
-    //     안녕하세요, PretendardVariable 입니다!
-    //   </Text>
-    //   <Text style={{ fontFamily: "RixInooAriDuriRegular", fontSize: 20 }}>
-    //     안녕하세요, RixInooAriDuriRegular 입니다!
-    //   </Text>
-    //   <StatusBar style="auto" />
-    // </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Main" component={Menu} />
+          <Stack.Screen name="AppPermissions" component={AppPermissions} />
+          <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicy} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#F5F5F5",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,21 +1,95 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Entypo } from "@expo/vector-icons"; // Entypo 아이콘 불러오기
-import Main from "../pages/Main";
-import Learning from "../pages/Learning";
-import MyPage from "../pages/MyPage";
-import { StyleSheet } from "react-native"; // StyleSheet import
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
+import Main from "../screens/Main";
+import Learning from "../screens/Learning";
+import MyPage from "../screens/MyPage";
+import { View, Image, StyleSheet } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 const Menu = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Main" component={Main} />
-      <Tab.Screen name="Learning" component={Learning} />
-      <Tab.Screen name="MyPage" component={MyPage} />
-    </Tab.Navigator>
+    <>
+      <View style={styles.Container}>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarStyle: styles.tabBar, // 스타일 적용
+            tabBarItemStyle: styles.tabItem, // 개별 아이템 스타일 적용
+            tabBarLabelStyle: styles.tabLabel, // 라벨 스타일 적용
+            showLabel: true, // 라벨 표시
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={Main}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Feather name="book-open" size={28} color="black" />
+              ),
+              tabBarLabel: "배움터",
+              headerShown: false, // 헤더 없애기
+            }}
+          />
+          <Tab.Screen
+            name="Learning"
+            component={Learning}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Image
+                  source={require("../../assets/images/SonsuLogo.png")}
+                  style={styles.centerIcon}
+                />
+              ),
+              tabBarLabel: "",
+            }}
+          />
+          <Tab.Screen
+            name="MyPage"
+            component={MyPage}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Feather name="user" size={28} color="black" />
+              ),
+              tabBarLabel: "마이페이지",
+            }}
+          />
+        </Tab.Navigator>
+      </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    bottom: 10,
+  },
+  tabBar: {
+    backgroundColor: "#fff",
+    height: 80,
+    borderRadius: 35,
+    shadowColor: "#000", // 그림자 색상
+    shadowOpacity: 0.1, // 그림자 투명도
+    shadowOffset: { width: 0, height: 3 }, // 그림자 위치 조정
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    zIndex: 1,
+  },
+  centerIcon: {
+    width: 90,
+    height: 90,
+    position: "absolute",
+    top: -40,
+  },
+  tabLabel: {
+    marginTop: 10,
+  },
+  tabItem: {
+    marginTop: 8,
+  },
+});
 
 export default Menu;
