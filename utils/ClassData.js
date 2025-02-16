7499ad85fa0c4ba1105026431406b91f9a9e61e6
cleame8922 <cleame8922@sungkyul.ac.kr>
2025-02-16 16:00:43 +0900

    [fix]classroom 카드컨테이너 및 잠금

--- utils/ClassData.js
+++ utils/ClassData.js
@@ -2,25 +2,28 @@ import SignImage from "../assets/images/Sign.png";
 
 const classData = {
   초급: [
-    { title: "지문자, 지숫자", animationPath: SignImage },
-    { title: "CSS 기초", animationPath: SignImage },
-    { title: "JavaScript 입문", animationPath: SignImage },
-    { title: "JavaScript 입문", animationPath: SignImage },
-    { title: "JavaScript 입문", animationPath: SignImage },
-    { title: "JavaScript 입문", animationPath: SignImage },
-    { title: "JavaScript 입문", animationPath: SignImage },
-    { title: "JavaScript 입문", animationPath: SignImage },
-    { title: "JavaScript 입문", animationPath: SignImage },
+    { id: 1, title: "지문자, 지숫자", animationPath: SignImage },
+    { id: 2, title: "인사 표현", animationPath: SignImage },
+    { id: 3, title: "감정 표현", animationPath: SignImage },
+    { id: 4, title: "날짜 및 시간", animationPath: SignImage },
+    { id: 5, title: "인칭 대명사", animationPath: SignImage },
+    { id: 6, title: "부사", animationPath: SignImage },
+    { id: 7, title: "명사", animationPath: SignImage },
+    { id: 8, title: "동사", animationPath: SignImage },
   ],
   중급: [
-    { title: "React 기초", animationPath: SignImage },
-    { title: "API 연동", animationPath: SignImage },
-    { title: "State 관리", animationPath: SignImage },
+    { id: 1, title: "인삿말", animationPath: SignImage },
+    { id: 2, title: "3인칭", animationPath: SignImage },
+    { id: 3, title: "날짜 및 시간", animationPath: SignImage },
+    { id: 4, title: "감정 표현", animationPath: SignImage },
+    { id: 5, title: "기타", animationPath: SignImage },
   ],
   고급: [
-    { title: "React Native", animationPath: SignImage },
-    { title: "Redux 활용", animationPath: SignImage },
-    { title: "성능 최적화", animationPath: SignImage },
+    { id: 1, title: "병원", animationPath: SignImage },
+    { id: 2, title: "법", animationPath: SignImage },
+    { id: 3, title: "속담", animationPath: SignImage },
+    { id: 4, title: "학교", animationPath: SignImage },
+    { id: 5, title: "기타", animationPath: SignImage },
   ],
 };
 
