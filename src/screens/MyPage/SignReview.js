d4360942adb69fd0f34b37d41ac4732c17071e2d
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-02-22 18:38:10 +0900

    [add]출석체크

---
+++ src/screens/MyPage/SignReview.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from "react-native";
+import Header from "../../components/Header";
+
+const SignReview = () => {
+  return (
+    <View>
+      <Header />
+      <Text>수어 다시보기</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SignReview;
